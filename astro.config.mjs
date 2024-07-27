9137db666bbe4a1066d26fd8bd92ab1229cdea1f
JLarky <jlarky@gmail.com>
2024-07-27 14:00:10 -0600

    add astro zeabur output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
+import zeabur from '@zeabur/astro-adapter/serverless';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+	adapter: zeabur(),
+	output: 'hybrid', // or 'server'
+	experimental: {
+		actions: true,
+	},
 });
